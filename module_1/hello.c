ab19fc71345516a748ddc51e7d0ae77544e9488e
David Kassin <david.j.kassin@gmail.com>
2024-08-29 16:36:08 -0600

    Add simple math program

---
+++ module_1/hello.c
@@ -0,0 +1,10 @@
+/* Hello world program */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+}
+
+/* C code compiles to machine code, 1s and 0s that are very fast*/
---
+++ module_1/simple_math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 5;
+    int b = 3;
+    int sum;
+
+    sum = a + b;
+
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+
+    return 0;
+}
